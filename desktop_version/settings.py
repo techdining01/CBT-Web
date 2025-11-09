b354bedeaa49651f75f8231baa033f0df28b69ec
techdining01 <amooade12@gmail.com>
2025-11-09 11:15:14 +0100

    Update settings.py

--- desktop_version/settings.py
+++ desktop_version/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0.8000']
 
 
 # Application definition
@@ -186,4 +186,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
