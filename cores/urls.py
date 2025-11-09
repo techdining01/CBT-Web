d6f1cdd3d8859ef62b7e468a5abcfb0bc3c576b3
techdining01 <amooade12@gmail.com>
2025-11-09 08:56:16 +0100

    Delete cores directory

--- cores/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('backup/', views.backup_database, name='backup_database'),
-    path('restore/', views.restore_database, name='restore_database'),
-    path('session-expired/', views.session_expired, name='session_expired'),    
-]
