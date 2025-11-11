1cc2f6d775012b197135c84591af99e2993adbde
techdining01 <amooade12@gmail.com>
2025-11-11 11:07:50 +0100

    allowed_host_tweak

--- desktop_version/settings.py
+++ desktop_version/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['cbt-web-g5xq.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
